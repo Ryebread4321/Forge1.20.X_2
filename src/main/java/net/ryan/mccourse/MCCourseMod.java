4ca06a863dd497b39dca890f29d3e920a16b9762
Ryebread4321 <ryanshi10@hotmail.com>
2024-10-10 17:45:26 +1100

    First Push!

--- src/main/java/net/ryan/mccourse/MCCourseMod.java
+++ src/main/java/net/ryan/mccourse/MCCourseMod.java
@@ -11,6 +11,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.ryan.mccourse.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -47,6 +48,8 @@ public class MCCourseMod {
     public MCCourseMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
--- src/main/java/net/ryan/mccourse/item/ModItems.java
+++ src/main/java/net/ryan/mccourse/item/ModItems.java
@@ -1,11 +1,21 @@
 package net.ryan.mccourse.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.ryan.mccourse.MCCourseMod;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, MCCourseMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
 }
