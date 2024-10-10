47dae650ceaea8d74b4a48d65e774b8cfdc6e592
Ryebread4321 <ryanshi10@hotmail.com>
2024-10-10 16:04:37 +1100

    First Push!

---
+++ src/main/java/net/ryan/mccourse/item/ModItems.java
@@ -0,0 +1,11 @@
+package net.ryan.mccourse.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.ryan.mccourse.MCCourseMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, MCCourseMod.MOD_ID);
+}
